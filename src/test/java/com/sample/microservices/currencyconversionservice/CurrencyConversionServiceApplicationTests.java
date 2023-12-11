b21421ff26d2abb2c2bf969ab0a0d2ec124c2d70
Amit Yadav <yadavamit006@gmail.com>
2023-12-11 13:44:26 +0530

    inital commit

---
+++ src/test/java/com/sample/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
