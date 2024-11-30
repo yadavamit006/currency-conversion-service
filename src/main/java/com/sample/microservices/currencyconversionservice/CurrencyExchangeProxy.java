a93fde66400afd13b430fdefe35d41ebefc04d11
Amit Yadav <yadavamit006@gmail.com>
2024-11-30 16:35:41 +0530

    changes

--- src/main/java/com/sample/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/sample/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange",url = "http://localhost:8000")
-@FeignClient(name = "currency-exchange")
+@FeignClient(name = "currency-exchange")//we give name application name
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
